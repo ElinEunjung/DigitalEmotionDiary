eb8d31e09c48c101a0a48054e0d358abb7d456c0
ElinEunjung <eunjungp615@gmail.com>
2024-12-15 23:08:36 +0900

    update Comment Repository

--- Data/Repositories/CommentRepository.cs
+++ Data/Repositories/CommentRepository.cs
@@ -1,14 +1,53 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using DigitalEmotionDiary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace DigitalEmotionDiary.Data.Repositories
-//{
-//	public class CommentRepository
-//	{
-//		private readonly DigitalEmotionDiaryDbContext _dbContext;
-//		private readonly
-	
-//}
+namespace DigitalEmotionDiary.Data.Repositories
+{
+	public class CommentRepository
+	{
+		private readonly DigitalEmotionDiaryDbContext _dbContext;
+		private readonly DiaryEntryRepository _diaryEntryRepository;
+
+		public CommentRepository(
+			DigitalEmotionDiaryDbContext dbContext,
+			DiaryEntryRepository diaryEntryRepository
+			)
+		{
+			_dbContext = dbContext;
+			_diaryEntryRepository = diaryEntryRepository;
+		}
+		public void CreateComment(Comment comment)
+		{
+			_dbContext.Comment.Add(comment);
+			_dbContext.SaveChanges();
+		}
+
+		public void UpdateComment(Comment comment)
+		{
+			_dbContext.Comment.Update(comment);
+			_dbContext.SaveChanges();
+		}
+
+		public void DeleteCommentById(long commentId)
+		{
+			var comment = _dbContext.Comment.Find(commentId);
+			if (comment != null)
+			{
+				_dbContext.Comment.Remove(comment);
+				_dbContext.SaveChanges();
+			}
+		}
+
+		public List<Comment> GetCommentsByDiaryEntry(int diaryEntryId)
+		{
+			return _dbContext.Comment
+				.Where(c => c.DiaryEntryId == diaryEntryId)
+				.ToList();
+		}
+	}
+}
