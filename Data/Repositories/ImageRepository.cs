9e773711a56b2819fd98d359b08ee8690845e8e8
ElinEunjung <eunjungp615@gmail.com>
2024-12-16 23:24:19 +0900

    update ImageRepository, on working with ImageService

---
+++ Data/Repositories/ImageRepository.cs
@@ -0,0 +1,53 @@
+﻿using DigitalEmotionDiary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalEmotionDiary.Data.Repositories
+{
+	public class ImageRepository
+	{
+		private readonly DigitalEmotionDiaryDbContext _dbContext;
+
+		public ImageRepository(DigitalEmotionDiaryDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			
+		}
+
+		public Image? GetImageById(long id)
+		{
+			return _dbContext.Image
+				.Include(img => img.DiaryEntry) // load image with DiaryEntry
+				.FirstOrDefault(img => img.Id == id);
+		}
+
+		public List<Image> GetImagesByEntryId(long entryId)
+		{
+			return _dbContext.Image
+				.Where(img => img.DiaryEntryId == entryId)
+				.ToList();
+		}
+
+		public void UploadEntryImage(Image image)
+		{
+			_dbContext.Image.Add(image);
+			_dbContext.SaveChanges();
+		}
+
+		public void DeleteImage(long id)
+		{
+			var image = _dbContext.Image.FirstOrDefault(img => img.Id == id);
+			_dbContext.Image.Remove(image);
+			_dbContext.SaveChanges();
+		}
+
+		public void SaveChanges()
+		{
+			_dbContext.SaveChanges();
+		}
+	}
+}
---
+++ Services/ImageService.cs
@@ -0,0 +1,34 @@
+﻿using DigitalEmotionDiary.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalEmotionDiary.Services
+{
+	public class ImageService
+	{
+		private readonly ImageRepository? _imageRepository;
+
+		public ImageService(ImageRepository? imageRepository)
+		{
+			_imageRepository = imageRepository;
+		}
+
+		public void UploadEntryImage(long userId, long entryId, string filePath, string? description = null)
+		{
+			
+		}
+
+		public void DeleteImage(long id)
+		{
+			_imageRepository.DeleteImage(id);
+			_imageRepository.SaveChanges();
+
+		}
+	}
+
+	
+}
