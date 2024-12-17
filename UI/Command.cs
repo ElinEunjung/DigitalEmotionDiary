4f57074e5d06b2a5b47cb5f1ab69b42895e6e8bc
Tor Tangenes <tcct89@hotmail.com>
2024-12-17 14:31:42 +0100

    Implement Command validation in UI

---
+++ UI/Command.cs
@@ -0,0 +1,60 @@
+using Microsoft.VisualBasic;
+
+class Command
+{
+    String _name;
+    String[] _arguments;
+    int _nrOfArguments;
+
+    public Command(String name, String[] arguments)
+    {
+        _name = name;
+        _arguments = arguments;
+        _nrOfArguments = _arguments.Length;
+    }
+    
+    public Command(String name, int nrOfArguments)
+    {
+        _name = name;
+        _nrOfArguments = nrOfArguments;
+    }
+
+    public bool IsQuitCommand()
+    {
+        return _name == "QUIT";
+    }
+    
+    public override bool Equals(object obj)
+    {        
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+        
+        Command otherCommand = (Command) obj;
+
+        return otherCommand._name == _name && otherCommand._nrOfArguments == _nrOfArguments;
+    }
+
+    public String GetName()
+    {
+        return _name;
+    }
+
+    public int GetNrOfArguments()
+    {
+        return _nrOfArguments;
+    }
+
+    
+    public String[] GetArguments()
+    {
+        return _arguments;
+    }
+
+    public static Command BlankCommand()
+    {
+        return new Command("", []);
+    }
+    
+}
--- UI/Menu.cs
+++ UI/Menu.cs
@@ -12,6 +12,18 @@ namespace DigitalEmotionDiary.UI
 		private readonly UserProfileUI _userProfileUI;
 		private bool _isLoggedIn = false;
 
+		private static String GET_COMMAND = "GET";
+    	private static String ADD_COMMAND = "ADD";
+    	private static String QUIT_COMMAND = "QUIT";
+
+    
+    	private static Dictionary<String, Command> validCommands = new Dictionary<string, Command>
+		{
+        	{ADD_COMMAND, new Command(ADD_COMMAND, 3)},
+        	{GET_COMMAND, new Command(GET_COMMAND, 1)},
+        	{QUIT_COMMAND, new Command(QUIT_COMMAND, 0)}
+    	};
+
 		public Menu(DiaryEntryUI diaryEntryUI, UserProfileUI userProfileUI, bool isLoggdIn)
 		{
 			_diaryEntryUI = diaryEntryUI;
@@ -35,12 +47,72 @@ namespace DigitalEmotionDiary.UI
 			}
 
 			DisplayMainMenu();
+			GetInputFromUser();
+			Console.WriteLine("Exiting program...");
 		}
 
 		private void DisplayMainMenu()
 		{
-			Console.WriteLine("1. Write Diary");
-			Console.WriteLine("2. Delete Diary");
+			Console.WriteLine("WRITE  - Write entry to Diary");
+			Console.WriteLine("DELETE - Delete entry from Diary");
+		}
+
+		private void GetInputFromUser()
+		{
+			var UserCommand = Command.BlankCommand();
+			while (!UserCommand.IsQuitCommand())
+			{
+				var userInput = Console.ReadLine();
+				UserCommand = ParseInputAsCommand(userInput);
+
+				if (CommandIsValid(UserCommand) && CommandHasCorrectNrOfArguments(UserCommand) && !UserCommand.IsQuitCommand())
+				{
+					ExecuteCommand(UserCommand);
+				}
+				else
+				{
+					Console.WriteLine("Error: Unrecognized command " + UserCommand.GetName());
+				}
+			}
+		}
+
+
+    	static Command ParseInputAsCommand(String userInput)
+		{
+			if (userInput.Length < 1)
+			{
+				return Command.BlankCommand();
+			}
+			var commandWithArguments = userInput.Split(" ");
+			String command = commandWithArguments[0];
+			String[] arguments = [];
+			if (commandWithArguments.Length > 1) {
+				arguments = new String[commandWithArguments.Length - 1];
+				Array.Copy(commandWithArguments, 1, arguments, 0, arguments.Length);
+			}
+			return new Command(command, arguments);
+		}
+			
+		static void ExecuteCommand(Command command)
+		{
+			Console.WriteLine("Executing command: " + command.GetName() + " with arguments " + String.Join(" ", command.GetArguments()) + " ...");
+		}
+
+		static bool CommandIsValid(Command command)
+		{
+			return validCommands.ContainsKey(command.GetName());
+		}
+
+		static bool CommandHasCorrectNrOfArguments(Command command)
+		{
+			var nrOfArgumentsInGivenCommand = command.GetNrOfArguments();
+			var nrOfArgumenntsInValidCommand = validCommands[command.GetName()].GetNrOfArguments();
+			if (nrOfArgumentsInGivenCommand != nrOfArgumenntsInValidCommand)
+			{
+				Console.WriteLine("Number of arguments given"  + "(" + nrOfArgumentsInGivenCommand + ")" + "in command " + command.GetName() + " differ from the valid number of arguments: " + nrOfArgumenntsInValidCommand);
+				return false;
+			}
+			return true;
 		}
 	}
 }
